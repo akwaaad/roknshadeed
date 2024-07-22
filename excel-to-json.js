92eb7bad9bc6895fbe72287485236b1d1a3741be
akwaaad <akwaaad@gmail.com>
2024-07-22 16:26:59 +0300

    update

--- excel-to-json.js
+++ excel-to-json.js
@@ -1,5 +1,6 @@
 const ExcelJS = require("exceljs");
 const fflate = require("fflate");
+const fs = require("fs");
 
 async function main() {
   const workbook = new ExcelJS.Workbook();
@@ -14,7 +15,19 @@ async function main() {
     sheetImages.forEach((image) => {
       let row = image.range.tl.nativeRow + 1;
       let col = image.range.tl.nativeCol + 1;
-      rows[row][col] = workbook.media[image.imageId].buffer.toString("base64");
+
+      let image_buf = workbook.media[image.imageId].buffer;
+      let image_md5 = require("crypto")
+        .createHash("md5")
+        .update(image_buf)
+        .digest("hex");
+      fs.writeFile(`./images/${image_md5}.png`, image_buf, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });
+      rows[row][col] = image_md5 + ".png";
     });
 
     const headerRow = rows[1];
